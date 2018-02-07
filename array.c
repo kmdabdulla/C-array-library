1b164ed0ab46a5de761d3c0025456551d223c949
Mohamed Abdulla <abdulla@jazenetworks.com>
2018-02-06 19:12:20 -0500

    array diff

--- array.c
+++ array.c
@@ -4,12 +4,12 @@
 
 int** array_chunk(int *array, int arraySize, int chunkSize) { //function to chunk the array with the given chunk size 
 	/*
-            *array - pointer to array to be chunked
-            arraySize - number of elements in the array
-            chunkSize - number of elements to be in each array chunk
-           
-            return a pointer to the two dimensional array which contains the chunked arrays 
-	*/
+	 *array - pointer to array to be chunked
+	 arraySize - number of elements in the array
+	 chunkSize - number of elements to be in each array chunk
+
+	 return a pointer to the two dimensional array which contains the chunked arrays 
+	 */
 	int i,j;         
 	float arrSize = (float)arraySize;
 	int row = (int)ceil(arrSize/chunkSize);
@@ -21,6 +21,37 @@ int** array_chunk(int *array, int arraySize, int chunkSize) { //function to chun
 		}
 	}
 	return data;
+}
+
+struct arr_diff array_diff(int *array1, int *array2, int array1_Size, int array2_Size) { //function to check the difference between two arrays
+
+	/* 
+
+	 *array1 - pointer to array to be comapared
+	 *array2 - pointer to array to compared with
+
+	 return a struct which contains difference array and its size*/
+
+	int i,j,flag=0,count=0;
+        struct arr_diff diff; 
+	int *data = malloc(array1_Size*sizeof(int));
+	for(i=0;i<array1_Size;i++) {
+		for(j=0;j<array2_Size;j++) {
+			if(array1[i]==array2[j]) {
+				flag=1;
+				break;
+			}
+		}
+		if(flag==0) {
+			*(data+count) = array1[i];
+			count++;
+		}
+		flag=0;
+	}
+        diff.data = data;
+        diff.arraySize = count;
+	return diff;
+	
 }
 
 
--- array.h
+++ array.h
@@ -2,8 +2,13 @@
 #ifndef _array__h
 #define _array__h
  
-/*Sorts an integer array. Takes a pointer to the first element and the length of the array as input. 
-Returns 0 on successful sort.*/
+struct arr_diff {
+
+        int *data;
+        int arraySize;
+
+};
+
 int** array_chunk(int*, int, int); 
- 
+struct arr_diff array_diff(int*, int*,int,int);
 #endif
